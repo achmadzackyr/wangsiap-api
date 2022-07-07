99be49257b942353420c2f824128dcfda95e501a
Achmad Zacky Rachmatullah <achmadzackyr@gmail.com>
2022-07-07 21:07:16 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\Api\CustomerController;
 use App\Http\Controllers\Api\CustomerStatusController;
 use App\Http\Controllers\Api\GatewayController;
@@ -13,22 +12,22 @@ use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(AuthController::class)->group(function () {
-    Route::post('/auth/register', 'register');
-    Route::post('/login', 'login');
-    Route::post('/auth/forgot-password', 'forgot');
-    Route::post('/auth/reset-password', 'reset');
+// Route::controller(AuthController::class)->group(function () {
+//     Route::post('/auth/register', 'register');
+//     Route::post('/login', 'login');
+//     Route::post('/auth/forgot-password', 'forgot');
+//     Route::post('/auth/reset-password', 'reset');
 
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::prefix('/auth/profile')->group(function () {
-            Route::get('/', 'profile');
-            Route::post('/', 'update');
-        });
+//     Route::middleware('auth:sanctum')->group(function () {
+//         Route::prefix('/auth/profile')->group(function () {
+//             Route::get('/', 'profile');
+//             Route::post('/', 'update');
+//         });
 
-        Route::post('/auth/logout', 'logout');
-    });
+//         Route::post('/auth/logout', 'logout');
+//     });
 
-});
+// });
 
 Route::post('auth/register', [PassportAuthController::class, 'register']);
 Route::post('auth/login', [PassportAuthController::class, 'login']);
