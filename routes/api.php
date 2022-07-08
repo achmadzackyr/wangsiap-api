22882ae5f15c5ca72e7308acf31bbe1e4139081a
Achmad Zacky Rachmatullah <achmadzackyr@gmail.com>
2022-07-08 09:18:35 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('/auth/register', 'register');
-    Route::post('/login', 'login');
+    Route::post('/auth/login', 'login');
     Route::post('/auth/forgot-password', 'forgot');
     Route::post('/auth/reset-password', 'reset');
 
